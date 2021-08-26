ec561d53169f56ffff7683449435c6df142381c8
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-08-26 15:46:47 +0600

    Update

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,21 +1,21 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://151.248.121.132:8920'
+  baseURL: 'https://api.dropmakers.org:8920'
 })
 
 export const auth = axios.create({
-  baseURL: 'http://151.248.121.132:8920/api/v1/auth',
+  baseURL: 'https://api.dropmakers.org:8920/api/v1/auth',
   headers: {
     Authorization: localStorage.getItem("token")
   }
 })
 
 export const ebay = axios.create({
-  baseURL: 'http://151.248.121.132:8920/ebay'
+  baseURL: 'https://api.dropmakers.org:8920/ebay'
 })
 
 export const payments = axios.create({
-  baseURL: 'http://151.248.121.132:8920/api/v1/payments'
+  baseURL: 'https://api.dropmakers.org:8920/api/v1/payments'
 })
 
